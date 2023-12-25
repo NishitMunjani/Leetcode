d2409aaf45ab75e789979cd7eaf89f9b4ea35c3d
M-Nishit <92155466+NishitMunjani@users.noreply.github.com>
2023-12-25 12:13:27 +0530

    Update 2Sum.cpp

--- Arrays/2Sum.cpp
+++ Arrays/2Sum.cpp
@@ -1,14 +1,16 @@
-#include<iostream>
-#include<vector>
-using namespace std;
-
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
+        map<int, int> mpp;
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            int num = nums[i];
+            int needed = target - num;
+            if(mpp.find(needed) != mpp.end()){
+                return {mpp[needed], i};
+            }
+            mpp[num] = i;
+        }
+        return {-1, -1};
     }
 };
-int main(){
-    vector<int>& nums;
-    return 0;
-}
