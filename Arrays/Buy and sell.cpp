ba18cd03bb9750dbb8272d4dbd1fd295e5c5d708
M-Nishit <92155466+NishitMunjani@users.noreply.github.com>
2023-12-28 21:42:03 +0530

    Upload Files

--- Arrays/Buy and sell.cpp
+++ Arrays/Buy and sell.cpp
@@ -1,8 +1,15 @@
-#include<iostream>
-#include<vector>
-using namespace std;
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini = INT_MAX;
+        int maxi = INT_MIN;
 
-int main(){
-    cout<<"1234";
-    return 0;
-}   
+        for(int i=0; i<prices.size(); i++){
+            mini = min(mini, prices[i]);
+            maxi = max(maxi, prices[i]- mini);
+        }
+
+        if(maxi!=0) return maxi;
+        else return 0;
+    }
+};
